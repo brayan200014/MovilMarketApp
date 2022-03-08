661526b6a6cacbc88df2374ea443494f77e53620
jete20 <78774120+jete20@users.noreply.github.com>
2022-03-08 08:50:57 -0600

    Pantalla de Facturacion

--- marketapp/App.js
+++ marketapp/App.js
@@ -1,20 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from 'react';
+import { ActivityIndicator, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+
+import Factura from './src/Pantallas/Facturar';
 
 export default function App() {
+  
+  const [assetsLoaded, setAssetsLoaded] = useState(false);
+
+  const _loadAssetsAsync = async () =>{
+    setAssetsLoaded(true);
+  }
+
+  useEffect(()=>{
+    _loadAssetsAsync();
+  });
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style = {{flex: 1}}>
+       <Factura />
+    </SafeAreaView>
+    
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
