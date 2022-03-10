ffcc688c71b5a3c9c6c6adcae2e394d82649b195
moises199802 <moisesdavid201@gmail.com>
2022-03-10 09:59:07 -0600

    InformacionM

--- marketapp/src/componentes/informacionApp.js
+++ marketapp/src/componentes/informacionApp.js
@@ -1,23 +1,56 @@
-import React from "react";
-import { View,Dimensions,StyleSheet,Text } from "react-native";
+import React, { Component } from 'react';
+import { StyleSheet, Image, View, Text} from 'react-native';
 
-const dish = Dimensions.get('window').height;
-const disw = Dimensions.get('window').width;
 
-class Información extends React.Component{
-    render(){
-        return(
-            <View>
-                <View style={styles.upperSections}>
-                    <Text>Secction 1 </Text>
-                    </View>
-                    <View style={styles.bottomSection}>
-                    <Text>Secction 2</Text>
+export default class Información extends Component  {
 
-            </View>
-            </View>
-        )
-    }
+  render(){
+    return(
+      <View>
+        <View style={styles.mainContainer}>
+          <Text style={styles.textTitulo}>Información</Text>
+      </View>
+      <View>
+      <Text style={styles.textStyle}>Super Mercado 1801</Text>
+      <Image style={styles.logo}
+      source={require('./assets/prueba.jpg')} />
+       <Text style={styles.principal}>La idea de creación del supermercado surgio ante las necesidades de los consumidores de realizar sus compras de manera rapida, comoda y economica y segura, utilizando las herramientas tecnológicas de la actualidad para dar satisfaccion a los consumidores. SUPERMERCADO 1801 su objetivo es mejorar y simplificar los procesos de pedidos, entregas, promociones, orden y servicio al usuario, cubrir una necesidad de forma inmediata y segura, proporcionando una buena experiencia al usuario durante su compra.</Text>
+      </View>
+      </View>
+     
+      
+    );
+  }
 }
-
-
+const styles = StyleSheet.create({
+  principal:{
+    color:'#091114',
+    padding: 20,
+    fontSize: 17,
+    textAlign:'justify'
+  },
+  mainContainer:{
+    marginTop:20,
+    backgroundColor:'#3EA5DB',
+    height: 80
+  },
+  textStyle:{
+    color:'#091114',
+    padding: 20,
+    fontSize: 20,
+    textAlign:'center',
+    fontWeight: 'bold'
+  },
+  textTitulo:{
+    color:'#f5ffed',
+    fontSize: 20,
+    textAlign:'center',
+    padding:15,
+  },
+  logo: {
+    width: 400,
+    height: 150,
+    resizeMode:'contain',
+    justifyContent: 'center'
+  }
+});
