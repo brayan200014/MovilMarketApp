4809c66e44b3be3c6584303b8abf779ab150effb
moises199802 <moisesdavid201@gmail.com>
2022-03-09 20:34:16 -0600

    EdiconPerfil

--- marketapp/App.js
+++ marketapp/App.js
@@ -6,21 +6,9 @@ import Factura from './src/Pantallas/Facturar';
 
 export default function App() {
   
-  const [assetsLoaded, setAssetsLoaded] = useState(false);
-
-  const _loadAssetsAsync = async () =>{
-    setAssetsLoaded(true);
-  }
-
-  useEffect(()=>{
-    _loadAssetsAsync();
-  });
   
-  return (
-    <SafeAreaView style = {{flex: 1}}>
-       <Factura />
-    </SafeAreaView>
+ 
     
-  );
+
 }
 
---
+++ marketapp/src/componentes/edicionPerfil.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { StyleSheet, View, Text} from 'react-native';
+import { Button } from 'react-native-elements';
+import { Input } from 'react-native-elements';
+
+export default () => {
+
+  return (
+    <View>
+      <View style={styles.seccion}>
+      <View> 
+      <Text style={styles.name}>Edición de Perfil</Text>
+      </View>
+      </View>
+      <View>
+      <>
+
+      <Input style= {styles.input}placeholder="Nombre :"/>
+      <Input style= {styles.input}placeholder="Apellido :"/>
+      <Input style= {styles.input}placeholder="Número teléfono :"/> 
+      <Input style= {styles.input}placeholder="Usuario :"/>
+      <Input style= {styles.input}placeholder="Contraseña :" secureTextEntry={true} />
+      <Input style= {styles.input}placeholder="Dirección :" />
+      
+    </>
+    <View style={styles.Button1}>
+      <Button 
+      title="Guardar Cambios">
+         </Button>
+      </View>
+     
+    </View>
+    <View style={styles.seccion2}>
+    </View>
+      
+    </View>
+   
+  );
+};
+
+const styles = StyleSheet.create({
+  name:{
+      color:"#FFFFFF",
+      fontSize:22,
+      marginLeft:110,
+      marginTop:40
+  },
+  seccion:{
+    backgroundColor: "#3EA5DB",
+    height:100,
+    borderRadius:30
+  },
+  input:{
+    marginTop:30,
+    width: 200,
+    height: 30,
+    borderColor:"#43519D"
+  },
+  Button1:{
+    alignItems: 'center',
+    borderRadius:10,
+    margin:30,
+    
+  },
+  seccion2:{
+    backgroundColor:"#3EA5DB",
+    height:30,
+    marginTop:30
+  }
+
+});
