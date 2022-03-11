448da4038b032b7db2a2a1e8518b01d9c05fb079
moises199802 <moisesdavid201@gmail.com>
2022-03-11 00:54:20 -0600

    EdicionP

--- marketapp/src/componentes/edicionPerfil.js
+++ marketapp/src/componentes/edicionPerfil.js
@@ -1,37 +1,101 @@
 import React from 'react';
-import { StyleSheet, View, Text} from 'react-native';
-import { Button } from 'react-native-elements';
-import { Input } from 'react-native-elements';
+import {View,Text,TextInput, SafeAreaView,TouchableOpacity,StyleSheet} from 'react-native';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Feather from 'react-native-vector-icons/Feather';
+import {useTheme, Title,Caption,} from 'react-native-paper';
+
 
 const edicionPerfil = () => {
-
+  const {colors} = useTheme();
   return (
-    <View>
+    <View style={{marginTop:70}}>
       <View style={styles.seccion}>
       <View> 
       <Text style={styles.name}>Edición de Perfil</Text>
       </View>
       </View>
-      <View>
-      <>
+      <View style={styles.action}>
+          <FontAwesome name="user-o" color={colors.text} size={30} />
+          <TextInput
+            placeholder="Nombre:"
+            placeholderTextColor="#666666"
+            autoCorrect={false}
+            style={[
+              styles.textInput,
+              {
+                color: colors.text,
+              },
+            ]}
+          />
+        </View>
 
-      <Input style= {styles.input}placeholder="Nombre :"/>
-      <Input style= {styles.input}placeholder="Apellido :"/>
-      <Input style= {styles.input}placeholder="Número teléfono :"/> 
-      <Input style= {styles.input}placeholder="Usuario :"/>
-      <Input style= {styles.input}placeholder="Contraseña :" secureTextEntry={true} />
-      <Input style= {styles.input}placeholder="Dirección :" />
-      
-    </>
-    <View style={styles.Button1}>
-      <Button 
-      title="Guardar Cambios">
-         </Button>
+      <View style={styles.action}>
+      <FontAwesome name="user-o" color={colors.text} size={30} />
+      <TextInput
+        placeholder="Apellido"
+        placeholderTextColor="#666666"
+        autoCorrect={false}
+        style={[
+          styles.textInput,
+          {
+            color: colors.text,
+          },
+        ]}
+      />
       </View>
-     
-    </View>
+      <View style={styles.action}>
+      <FontAwesome name="user-o" color={colors.text} size={30} />
+      <TextInput
+        placeholder="Usuario"
+        placeholderTextColor="#666666"
+        autoCorrect={false}
+        style={[
+          styles.textInput,
+          {
+            color: colors.text,
+          },
+        ]}
+      />
+      </View>
+      <View style={styles.action}>
+          <Feather name="phone" color={colors.text} size={30} />
+          <TextInput
+            placeholder="Teléfono"
+            placeholderTextColor="#666666"
+            keyboardType="number-pad"
+            autoCorrect={false}
+            style={[
+              styles.textInput,
+              {
+                color: colors.text,
+              },
+            ]}
+          />
+        </View>
+        <View style={styles.action}>
+          <FontAwesome name="lock" color={colors.text} size={30} />
+          <TextInput
+            placeholder="Contraseña"
+            placeholderTextColor="#666666"
+            returnKeyType='go'
+            secureTextEntry
+            autoCorrect={false}
+            style={[
+              styles.textInput,
+              {
+                color: colors.text,
+              },
+            ]}
+          />
+          <TouchableOpacity style={styles.Sbutton} onPress={() => {}}>
+          <Text style={styles.ButtonPass}>Cambiar contraseña</Text>
+        </TouchableOpacity>
+        </View>
     <View style={styles.seccion2}>
     </View>
+    <TouchableOpacity style={styles.commandButton} onPress={() => {}}>
+          <Text style={styles.panelButtonTitle}>Guardar cambios</Text>
+        </TouchableOpacity>
       
     </View>
    
@@ -42,31 +106,77 @@ export default edicionPerfil;
 const styles = StyleSheet.create({
   name:{
       color:"#FFFFFF",
-      fontSize:22,
+      fontSize:18,
       marginLeft:110,
-      marginTop:40
   },
   seccion:{
+   margin:10,
     backgroundColor: "#3EA5DB",
-    height:100,
-    borderRadius:30
-  },
-  input:{
-    marginTop:30,
-    width: 200,
-    height: 30,
-    borderColor:"#43519D"
-  },
-  Button1:{
-    alignItems: 'center',
-    borderRadius:10,
-    margin:30,
-    
+    height:60,
+    borderRadius:30,
+    justifyContent:'center',
+    marginTop:25
   },
   seccion2:{
-    backgroundColor:"#3EA5DB",
+    backgroundColor:"#FF6347",
     height:30,
     marginTop:30
-  }
+  },
+  commandButton: {
+    justifyContent:'center',
+    padding: 15,
+    borderRadius: 10,
+    backgroundColor: '#3EA5DB',
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  Sbutton: {
+    backgroundColor:'#3EA5DB',
+  width:200,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent:'center'
+  },
+  
+  panelHandle: {
+    width: 40,
+    height: 8,
+    borderRadius: 4,
+    backgroundColor: '#00000040',
+    marginBottom: 10,
+  },
+
+  panelButtonTitle: {
+    fontSize: 17,
+    fontWeight: 'bold',
+    color: '#FFFFFF',
+  },
+  ButtonPass: {
+    fontSize: 12,
+    color: '#FFFFFF',
+    justifyContent:'center',
+    borderRadius:10,
+    fontWeight:'bold'
+  },
+  action: {
+    flexDirection: 'row',
+    marginTop: 30,
+    marginBottom: 30,
+    borderBottomWidth: 1,
+    borderBottomColor: '#f2f2f2',
+    paddingBottom: 5,
+  },
+  actionError: {
+    flexDirection: 'row',
+    marginTop: 10,
+    borderBottomWidth: 2,
+    borderBottomColor: '#FF0000',
+    paddingBottom: 5,
+  },
+  textInput: {
+    flex: 1,
+    paddingLeft: 10,
+    color: '#05375a',
+  },
 
 });
